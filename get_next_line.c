1c283dd12374e9e4e46105275c81b4ec09062ac4
Fargesia01 <87009346+Fargesia01@users.noreply.github.com>
2021-11-16 00:45:13 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -11,28 +11,56 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-int		check_tmp(char *tmp);
+int	check_tmp(char *tmp);
 char	*cpy_tmp(char *tmp, char *tab);
+char	*save_tmp(char *tmp);
 
 char	*get_next_line(int fd)
 {
 	char		*tab;
 	char		*tmp;
-	int			fin;
-	static int	ret;
+	int		fin;
+	static char	*save;
 
 	fin = 0;
-	tmp = malloc(4 * sizeof(char) + 1);
+	tmp = malloc(100 * sizeof(char) + 1);
 	tab = "\0";
+	if (save)
+		tab = cpy_tmp(save, tab);
 	while (!check_tmp(tmp))
 	{
-		fin += read(fd, tmp, 4);
+		fin += read(fd, tmp, 100);
 		tab = cpy_tmp(tmp, tab);
 	}
-	ret = fin - check_tmp(tmp) + 1;
-	printf("%d\n", ret);
+	save = save_tmp(tmp);
 	tab[fin] = 0;
 	return (tab);
+}
+
+char	*save_tmp(char *tmp)
+{
+	int	c;
+	int	c2;
+	char	*save;
+
+	c = 0;
+	while (tmp[c] != '\n')
+		c++;
+	while (tmp[c + c2])
+		c2++;
+	save = malloc(c2 * sizeof(char) + 1);
+	c = 0;
+	c2 = 0;
+	while (tmp[c] != '\n')
+		c++;
+	c++;
+	while (tmp[c])
+	{
+		save[c2] = tmp[c];
+		c2++;
+		c++;
+	}
+	return (save);
 }
 
 char	*cpy_tmp(char *tmp, char *tab)
@@ -68,21 +96,13 @@ char	*cpy_tmp(char *tmp, char *tab)
 int	check_tmp(char *tmp)
 {
 	int	c;
-	int	c2;
 
-	c2 = 0;
 	c = 0;
 	while (tmp[c])
 	{
 		if (tmp[c] == '\n')
 		{
-			c++;
-			while (tmp[c])
-			{
-				c2++;
-				c++;
-			}
-			return (c2);
+			return (1);
 		}	
 		c++;
 	}
@@ -94,12 +114,10 @@ int	main()
 	int fd;
 	char *tab;
 
-	//printf("%s\n", cpy_tmp("oooo\nkkkkkkkk\nllllllll\n\n", "\0"));
-
 	fd = open("test", O_RDWR | O_APPEND);
 	//tab = get_next_line(fd);
 	tab = get_next_line(fd);
 	tab = get_next_line(fd);
-	printf("%s", tab);
+	printf("%s\n", tab);
 	close(fd);
 }
